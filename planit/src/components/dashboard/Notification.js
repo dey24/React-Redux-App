844c5d54be519ae7bf074647eaee19bd948122c3
Chiranjit Dey <chiranjitdey2000@gmail.com>
2020-10-27 22:32:03 +0530

    setting up the dashboard

---
+++ planit/src/components/dashboard/Notification.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notification = () => {
+    return(
+        <div>
+            <h3 className="grey-text text-darken-4">Notification</h3>
+        </div>
+    );
+}
+
+export default Notification;
