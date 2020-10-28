9b721e4ca9ab9f249a457e55fa4abbe4095351a5
Chiranjit Dey <chiranjitdey2000@gmail.com>
2020-10-28 22:09:01 +0530

    using store

--- planit/src/components/projects/ProjectSummary.js
+++ planit/src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ProjectSummary = () =>{
+const ProjectSummary = ({project}) =>{
     return (
         <div className="card z-depth-5 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{project.title}</span>
                 <p>Posted by Cj</p>
                 <p className="grey-text">21st Oct, 2020</p>
             </div>
--- planit/src/components/store/reducers/projectReducer.js
+++ planit/src/components/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'What do you wanna do in life?', content: 'Become a top developer'},
+        {id: '2', title: 'What is your dream location?', content: 'Orlando, Florida'},
+        {id: '3', title: 'Will you ever keep a pet?', content: 'Ofc, I would love to.'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
